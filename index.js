f486c4dcdce788e133e2ee95f2d9971fd08c6321
Koki Ito <kokiito0926@gmail.com>
2026-02-08 11:58:42 +0900

    feat: 🎸 プレイグラウンドからファイルをコピーした

---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+// >> $ curl -sSL https://example.com/ | ./index.js
+
+import open from "open";
+import { stdin } from "zx";
+import express from "express";
+
+if (process.stdin.isTTY) {
+	process.exit(1);
+}
+
+const html = await stdin();
+if (!html) {
+	process.exit(1);
+}
+
+const app = express();
+const server = app.listen(3000, async () => {
+	const port = server.address().port;
+	const url = `http://localhost:${port}`;
+
+	app.get("/", (req, res) => {
+		res.send(html);
+
+		setTimeout(() => {
+			server.close(() => {
+				process.exit(0);
+			});
+		}, 300);
+	});
+
+	await open(url);
+});
